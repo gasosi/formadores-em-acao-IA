55d5d01f31815e049b931e5510427d9e729eb272
Gabriel Soares <115032490+gasosi@users.noreply.github.com>
2024-06-26 19:06:00 -0300

    sadasd

---
+++ script.js
@@ -0,0 +1,31 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+{
+    enunciado: "Pergunta1",
+    alternativas: [
+        "alternativa1", "alternativa2"
+    ]
+}
+
+{
+    enunciado: "Pergunta1",
+    alternativas: [
+        "alternativa1", "alternativa2"
+    ]
+}
+
+{
+    enunciado: "Pergunta1",
+    alternativas: [
+        "alternativa1", "alternativa2"
+    ]
+}
+
+
+
+]
